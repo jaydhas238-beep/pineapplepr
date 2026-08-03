dc5e0b50205c320d515ebeb33795678dfbfc68d3
jaydhas238-beep <jaydhas238@gmail.com>
2026-08-04 00:41:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,36 @@ faqItems.forEach(item => {
     });
 
 });
+
+document
+.getElementById("whatsappForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const service = document.getElementById("service").value;
+    const message = document.getElementById("message").value;
+
+    const text =
+`*New Website Enquiry*
+
+👤 Name: ${name}
+
+📧 Email: ${email}
+
+📱 Phone: ${phone}
+
+💼 Service: ${service}
+
+📝 Message:
+${message}`;
+
+    window.open(
+        `https://wa.me/917972219776?text=${encodeURIComponent(text)}`,
+        "_blank"
+    );
+
+});
